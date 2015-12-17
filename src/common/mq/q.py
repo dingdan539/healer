bc0d5edd8b7d97fdf78ab7a5d7b4a8b1b3d91ed3
dingdan <dingdan@yhd.123>
2015-12-17 14:57:23 +0800

    mq

--- src/common/mq/q.py
+++ src/common/mq/q.py
@@ -12,7 +12,7 @@ class Q(object):
 
     def __init__(self, tag, **kwargs):
         credentials = pika.PlainCredentials('opsdev', 'opsdev')
-        self.__connection = pika.BlockingConnection(pika.ConnectionParameters('10.4.1.226', '5672', '/', credentials))
+        self.__connection = pika.BlockingConnection(pika.ConnectionParameters('10.4.1.226', 5672, '/', credentials))
         self.__channel = self.__connection.channel()
         self.__channel.queue_declare(queue='aa_test')
 
