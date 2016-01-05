8a0cb9f5d94a9a6e2f67ee0dd7e3546054a42df2
dingdan <dingdan@yhd.123>
2016-01-05 17:19:39 +0800

    update

--- src/main_event/pattern_process.py
+++ src/main_event/pattern_process.py
@@ -13,19 +13,17 @@ class ProcessZabbixTomcat(Father, InterfaceOutPut):
         ip = warning_dict['ip']
         if (type_id == 4) and (status == 'PROBLEM'):
             cmdstr = r'''nc -z -vv -w 1 ''' + ip + ''' 8080'''
-            #code, stdout, stderr = fb.command(cmdstr)
-            print fb.command(cmdstr)
-            # if 'succeeded' not in stdout:
-            #     time.sleep(0.1)
-            #     code2, stdout2, stderr2 = fb.command(cmdstr)
-            #     print stdout2
-            #     if 'succeeded' not in stdout2:
-            #         warning_dict['remark'] = stdout2
-            #         kwargs = {
-            #             'tb_name': 'important_event',
-            #             'field': warning_dict
-            #         }
-            #         self.f_ie_db.insert(**kwargs)
+            code, stdout, stderr = fb.command(cmdstr)
+            if code == 1:
+                time.sleep(0.1)
+                code, stdout, stderr = fb.command(cmdstr)
+                if code == 1:
+                    warning_dict['remark'] = stderr
+                    kwargs = {
+                        'tb_name': 'important_event',
+                        'field': warning_dict
+                    }
+                    self.f_ie_db.insert(**kwargs)
             return True
         else:
             return False
