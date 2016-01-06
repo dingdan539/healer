c7cc795605d8889412aff6264eafea7a9e3e1499
dingdan <dingdan@yhd.123>
2016-01-06 15:08:55 +0800

    update

--- src/config/q/qmaps.py
+++ src/config/q/qmaps.py
@@ -4,13 +4,13 @@
         目前没有实现交换机和路由器，以后需要再改进
 """
 qmaps = {
-    """收集存放所有报警信息队列"""
+
     'zabbix_event_queue': {
         'prefix': 'RMQ_',
         'exchange': '',  # 定义交换机
         'route': ''      # 定义路由关键字
     },
-    """稳定性报警专用处理队列"""
+
     'zabbix_stability_queue': {
         'prefix': 'RMQ_',
         'exchange': '',  # 定义交换机
