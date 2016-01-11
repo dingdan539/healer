2de5dd7daf52bb44d2e81f9ba5a04112c95e31aa
dingdan <dingdan@yhd.123>
2016-01-11 17:10:35 +0800

    update

--- src/config/deploy.py
+++ src/config/deploy.py
@@ -1,7 +1,7 @@
 class Config(object):
     AUTHOR = "dingdan"
     ENV = "deploy"
-    DEBUG = True
+    DEBUG = False
 
 
 class DeployConfig(Config):
--- src/main.py
+++ src/main.py
@@ -12,21 +12,13 @@ if __name__ == "__main__":
     sys.path.append(ROOT_PATH)
 
     from src.main_event.assemble import *
-    from src.common.db.op import *
     from src.common.rabbitmq import consumer
 
-    #cum = consumer.Consumer('zabbix_event_queue')
-
-    warning_dict = dict()
-    warning_dict['description'] = """10.4.11.82PROBLEM: tomcat port 8080 can't connectvalue:0;_Trigger: tomcat port 8080 can't connect"""
-    # 10.4.11.82 10.4.43.176
-    warning_dict['clock'] = 12345678
-    warning_dict['host_name'] = " empty"
-    warning_dict['source_id'] = 1
+    cum = consumer.Consumer('zabbix_event_queue')
 
     a = ZabbixAnalyse()
-    a.analyse(warning_dict)
-    # def fun(body):
-    #     a.analyse(body)
-    #
-    # cum.receive(fun)
+
+    def fun(body):
+        a.analyse(body)
+
+    cum.receive(fun)
