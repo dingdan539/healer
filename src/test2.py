e98fb077fae736b1778378c525637ab5eb063f27
dingdan <dingdan@yhd.123>
2016-01-15 15:00:18 +0800

    update

--- src/test2.py
+++ src/test2.py
@@ -4,9 +4,8 @@ import sys
 import pprint
 from config import *
 import function.basic as fb
-
+from src.common.rabbitmq import publisher
 if __name__ == "__main__":
-    from src.common.rabbitmq import publisher
 
     a = publisher.Publisher('zabbix_event_queue')
     aaa = 'sadasdasd'
