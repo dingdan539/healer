582fa2f49381775761ed2a1c6ab7440657205541
dingdan <dingdan@yhd.123>
2016-01-15 15:03:00 +0800

    update

--- src/test2.py
+++ src/test2.py
@@ -4,8 +4,15 @@ import sys
 import pprint
 from config import *
 import function.basic as fb
-from src.common.rabbitmq import publisher
+
 if __name__ == "__main__":
+    config = load_config_auto()
+
+    ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    sys.path.append(ROOT_PATH)
+
+    from src.common.rabbitmq import publisher
 
     a = publisher.Publisher('zabbix_event_queue')
     aaa = 'sadasdasd'
