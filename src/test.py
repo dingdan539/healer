e99fa1fc09ab0544675df5612dea74cb2ab7bf42
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 13:26:13 +0800

    quanbu

--- src/test.py
+++ src/test.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-from src.main_event.pattern_process import *
+
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(ROOT_PATH)
+
+from src.main_event.pattern_process import *
 
 warning_dict = {'ip': '10.4.11.82','description':'111'}
 a = Main()
