85cab14908cc5977db9ce4f8bd1c7bc063e35868
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 14:02:08 +0800

    quanbu

--- src/test.py
+++ src/test.py
@@ -1,12 +1,15 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
+import json
 
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(ROOT_PATH)
 
 from src.main_event.pattern_process import *
+from src.common.redis import queue
 
-warning_dict = {'ip': '10.4.11.82','description':'111'}
-a = Main()
-a.nc_check(warning_dict, '', 8080, 2)
+cum = queue.InitQ('zabbix_event_queue')
+
+warning_dict = {'ip': '10.4.11.82','description':'111','type_id':4,'kind_id':1,'status':'PROBLEM'}
+print cum.rpush(json.dumps(warning_dict))
