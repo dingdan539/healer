25d5ff1d409a873a43146ea666a52d8097868aa4
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 14:21:24 +0800

    quanbu

--- src/test.py
+++ src/test.py
@@ -11,6 +11,6 @@ from src.common.redis import queue
 
 cum = queue.InitQ('zabbix_event_queue')
 
-warning_dict = {'ip': '10.4.11.82','description':'10.4.11.82PROBLEM: tomcat port 8080','type_id':4,'kind_id':1,'status':'PROBLEM'}
+warning_dict = {'ip': '10.4.11.82','description':'10.4.11.82PROBLEM: tomcat port 8080','type_id':4,'kind_id':1,'source_id':1,'status':'PROBLEM'}
 print warning_dict
 print cum.rpush(json.dumps(warning_dict))
