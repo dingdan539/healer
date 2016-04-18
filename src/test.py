27aad1d2597575cd9fbd048add69b8e9b412f5b2
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 16:48:50 +0800

    a

--- src/test.py
+++ src/test.py
@@ -8,11 +8,9 @@ sys.path.append(ROOT_PATH)
 
 from src.main_event.pattern_process import *
 from src.common.redis import queue
-print type((1, 'a'))
-a = (1, 'a')
-print a[0]
-# cum = queue.InitQ('zabbix_event_queue')
-#
-# warning_dict = {'description':'10.4.11.82PROBLEM: tomcat port 8080','source_id':1,'clock':1460962171}
-# print warning_dict
-# print cum.rpush(json.dumps(warning_dict))
+
+cum = queue.InitQ('zabbix_event_queue')
+
+warning_dict = {'description':'10.4.11.82PROBLEM: tomcat port 8080','source_id':1,'clock':1460962171}
+print warning_dict
+print cum.rpush(json.dumps(warning_dict))
