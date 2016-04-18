22bce72e7378305b021441f73210891319a42522
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 13:48:06 +0800

    quanbu

--- src/main_event/pattern_process.py
+++ src/main_event/pattern_process.py
@@ -46,25 +46,14 @@ class ProcessZabbixFilter(Father, InterfaceOutPut):
             return False
 
 
-class ProcessZabbixTomcat(Father, InterfaceOutPut):
+class ProcessZabbixTomcat(Main, InterfaceOutPut):
     """必须有返回bool True - 执行并break False - 寻找下一个执行点"""
     def process(self, warning_dict):
         type_id = warning_dict['type_id']
         status = warning_dict['status']
         ip = warning_dict['ip']
         if (type_id == 4) and (status == 'PROBLEM'):
-            cmdstr = r'''nc -z -vv -w 1 ''' + ip + ''' 8080'''
-            code, stdout, stderr = fb.command(cmdstr)
-            if code == 1:
-                time.sleep(0.1)
-                code, stdout, stderr = fb.command(cmdstr)
-                if code == 1:
-                    warning_dict['remark'] = stderr
-                    kwargs = {
-                        'tb_name': 'important_event',
-                        'field': warning_dict
-                    }
-                    self.f_ie_db.insert(**kwargs)
+            self.nc_check(warning_dict, ip, 8080, 3)
             return True
         else:
             return False
