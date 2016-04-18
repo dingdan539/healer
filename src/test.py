46753e187b42942f0b3f9f406c75312d911281c7
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-18 14:09:32 +0800

    quanbu

--- src/test.py
+++ src/test.py
@@ -12,4 +12,5 @@ from src.common.redis import queue
 cum = queue.InitQ('zabbix_event_queue')
 
 warning_dict = {'ip': '10.4.11.82','description':'111','type_id':4,'kind_id':1,'status':'PROBLEM'}
+print warning_dict
 print cum.rpush(json.dumps(warning_dict))
