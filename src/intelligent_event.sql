386ed5c76235aaa9e6de2665e3eb9b7ed74a98db
unknown <dingdan@NH01ITNB7001126.yihaodian.com>
2016-04-26 18:38:12 +0800

    aa

--- src/intelligent_event.sql
+++ src/intelligent_event.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.60 
-MySQL - 5.6.17 : Database - intelligent_event
+MySQL - 5.6.12-log : Database - intelligent_event
 *********************************************************************
 */
 
@@ -16,11 +16,39 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`intelligent_event` /*!40100 DEFAULT CHA
 
 USE `intelligent_event`;
 
-/*Table structure for table `alert_2015` */
+/*Table structure for table `alert_2016` */
 
-DROP TABLE IF EXISTS `alert_2015`;
+DROP TABLE IF EXISTS `alert_2016`;
 
-CREATE TABLE `alert_2015` (
+CREATE TABLE `alert_2016` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) NOT NULL DEFAULT '',
+  `site_id` int(11) NOT NULL DEFAULT '0',
+  `pool_id` int(11) NOT NULL DEFAULT '0',
+  `host_name` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `kind_id` int(11) NOT NULL DEFAULT '0',
+  `type_id` int(11) NOT NULL DEFAULT '0',
+  `level_id` int(11) NOT NULL DEFAULT '0',
+  `source_id` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(255) NOT NULL DEFAULT '',
+  `remark` varchar(255) NOT NULL DEFAULT '',
+  `server_type_id` int(11) NOT NULL DEFAULT '0',
+  `rack_id` int(11) NOT NULL DEFAULT '0',
+  `switch_ip` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `clock` (`clock`) USING BTREE,
+  KEY `ip` (`ip`) USING BTREE,
+  KEY `pool_id` (`pool_id`) USING BTREE,
+  KEY `site_id` (`site_id`,`pool_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=23618 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `alert_2017` */
+
+DROP TABLE IF EXISTS `alert_2017`;
+
+CREATE TABLE `alert_2017` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(255) NOT NULL DEFAULT '',
   `site_id` int(11) NOT NULL DEFAULT '0',
@@ -41,13 +69,11 @@ CREATE TABLE `alert_2015` (
   KEY `site_id` (`site_id`,`pool_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `alert_2015` */
+/*Table structure for table `alert_2018` */
 
-/*Table structure for table `alert_2016` */
+DROP TABLE IF EXISTS `alert_2018`;
 
-DROP TABLE IF EXISTS `alert_2016`;
-
-CREATE TABLE `alert_2016` (
+CREATE TABLE `alert_2018` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(255) NOT NULL DEFAULT '',
   `site_id` int(11) NOT NULL DEFAULT '0',
@@ -55,20 +81,24 @@ CREATE TABLE `alert_2016` (
   `host_name` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL,
   `clock` int(11) NOT NULL DEFAULT '0',
-  `type` int(11) NOT NULL DEFAULT '0',
-  `source` int(11) NOT NULL DEFAULT '0',
+  `kind_id` int(11) NOT NULL DEFAULT '0',
+  `type_id` int(11) NOT NULL DEFAULT '0',
+  `level_id` int(11) NOT NULL DEFAULT '0',
+  `source_id` int(11) NOT NULL DEFAULT '0',
   `status` varchar(255) NOT NULL DEFAULT '',
   `remark` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `clock` (`clock`) USING BTREE,
+  KEY `ip` (`ip`) USING BTREE,
+  KEY `pool_id` (`pool_id`) USING BTREE,
+  KEY `site_id` (`site_id`,`pool_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `alert_2016` */
+/*Table structure for table `alert_2019` */
 
-/*Table structure for table `alert_2017` */
+DROP TABLE IF EXISTS `alert_2019`;
 
-DROP TABLE IF EXISTS `alert_2017`;
-
-CREATE TABLE `alert_2017` (
+CREATE TABLE `alert_2019` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(255) NOT NULL DEFAULT '',
   `site_id` int(11) NOT NULL DEFAULT '0',
@@ -76,14 +106,57 @@ CREATE TABLE `alert_2017` (
   `host_name` varchar(255) NOT NULL DEFAULT '',
   `description` text NOT NULL,
   `clock` int(11) NOT NULL DEFAULT '0',
-  `type` int(11) NOT NULL DEFAULT '0',
-  `source` int(11) NOT NULL DEFAULT '0',
+  `kind_id` int(11) NOT NULL DEFAULT '0',
+  `type_id` int(11) NOT NULL DEFAULT '0',
+  `level_id` int(11) NOT NULL DEFAULT '0',
+  `source_id` int(11) NOT NULL DEFAULT '0',
   `status` varchar(255) NOT NULL DEFAULT '',
   `remark` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `clock` (`clock`) USING BTREE,
+  KEY `ip` (`ip`) USING BTREE,
+  KEY `pool_id` (`pool_id`) USING BTREE,
+  KEY `site_id` (`site_id`,`pool_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `alert_2017` */
+/*Table structure for table `alert_2020` */
+
+DROP TABLE IF EXISTS `alert_2020`;
+
+CREATE TABLE `alert_2020` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(255) NOT NULL DEFAULT '',
+  `site_id` int(11) NOT NULL DEFAULT '0',
+  `pool_id` int(11) NOT NULL DEFAULT '0',
+  `host_name` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `kind_id` int(11) NOT NULL DEFAULT '0',
+  `type_id` int(11) NOT NULL DEFAULT '0',
+  `level_id` int(11) NOT NULL DEFAULT '0',
+  `source_id` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(255) NOT NULL DEFAULT '',
+  `remark` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `clock` (`clock`) USING BTREE,
+  KEY `ip` (`ip`) USING BTREE,
+  KEY `pool_id` (`pool_id`) USING BTREE,
+  KEY `site_id` (`site_id`,`pool_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `api_log` */
+
+DROP TABLE IF EXISTS `api_log`;
+
+CREATE TABLE `api_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from_ip` varchar(255) NOT NULL DEFAULT '',
+  `path` varchar(255) NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `date` int(11) NOT NULL,
+  `remark` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=631 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `important_event` */
 
@@ -99,13 +172,18 @@ CREATE TABLE `important_event` (
   `clock` int(11) NOT NULL DEFAULT '0' COMMENT '默认or直接得到',
   `kind_id` int(11) NOT NULL DEFAULT '0',
   `type_id` int(11) NOT NULL DEFAULT '0' COMMENT '计算',
+  `level_id` int(11) NOT NULL DEFAULT '0',
   `source_id` int(11) NOT NULL DEFAULT '0' COMMENT '直接得到',
   `status` varchar(255) NOT NULL DEFAULT '' COMMENT '计算',
   `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '计算or直接得到',
+  `enable` int(11) NOT NULL DEFAULT '0' COMMENT '0-未处理',
+  `server_type_id` int(11) NOT NULL DEFAULT '0',
+  `rack_id` int(11) NOT NULL DEFAULT '0',
+  `switch_ip` varchar(255) NOT NULL DEFAULT '',
+  `probably_id` int(11) NOT NULL DEFAULT '0',
+  `probably_reason` varchar(255) DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `important_event` */
+) ENGINE=InnoDB AUTO_INCREMENT=1533 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `kind_map` */
 
@@ -117,10 +195,6 @@ CREATE TABLE `kind_map` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `kind_map` */
-
-insert  into `kind_map`(`id`,`name`) values (1,'可用性'),(2,'稳定性');
-
 /*Table structure for table `level_map` */
 
 DROP TABLE IF EXISTS `level_map`;
@@ -130,10 +204,6 @@ CREATE TABLE `level_map` (
   `name` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
-/*Data for the table `level_map` */
-
-insert  into `level_map`(`id`,`name`) values (1,'网络层'),(2,'系统层'),(3,'中间件层'),(4,'应用层'),(5,'业务层');
 
 /*Table structure for table `shield` */
 
@@ -148,14 +218,10 @@ CREATE TABLE `shield` (
   `source_id` int(11) NOT NULL DEFAULT '0',
   `start_time` int(11) NOT NULL DEFAULT '0',
   `end_time` int(11) NOT NULL DEFAULT '0',
-  `expire_time` int(11) NOT NULL DEFAULT '0',
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0-屏蔽中 1-已取消屏蔽',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-/*Data for the table `shield` */
-
-insert  into `shield`(`id`,`pool_id`,`ip`,`key`,`level_id`,`source_id`,`start_time`,`end_time`,`expire_time`,`status`) values (1,486,'1','',0,0,12345670,12345679,12345699,0);
+  `enable` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `end_time` (`enable`,`start_time`,`end_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Table structure for table `source_map` */
 
@@ -166,10 +232,6 @@ CREATE TABLE `source_map` (
   `name` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-/*Data for the table `source_map` */
-
-insert  into `source_map`(`id`,`name`) values (1,'zabbix'),(2,'healthcheck');
 
 /*Table structure for table `type_map` */
 
@@ -183,9 +245,17 @@ CREATE TABLE `type_map` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
 
-/*Data for the table `type_map` */
+/*Table structure for table `user_token` */
 
-insert  into `type_map`(`id`,`name`,`level_id`,`kind_id`) values (1,'traffic utilization',1,2),(2,'netstat established',1,2),(3,'cpu utilization',2,2),(4,'tomcat port 8080',4,1),(5,'disk space',2,1),(6,'load',2,2),(9,'zabbix agent',3,1),(10,'order number',5,1),(11,'squid',3,1),(12,'mysql port',4,1),(13,'cms',4,1),(14,'cdn',1,2),(15,'memcache',3,1),(16,'swap',2,2),(17,'oracle',4,1),(18,'zk',4,1),(19,'network established',1,2),(20,'webserver port',4,1),(21,'disk I/O',2,2),(22,'network time wait',1,2),(23,'dts java',4,1),(24,'tomcat full gc',4,1),(25,'redis',3,1),(26,'netstat timewait',1,2),(27,'mem',3,1),(28,'mongo',3,1),(29,'JMX',4,1),(30,'free inodes',2,2),(31,'SSLVPN',1,1),(32,'python',4,1),(33,'ping',1,1),(34,'jumper full gc',4,1),(35,'traffic changed a lot',1,1),(36,'zookeeper',4,1),(37,'service sequence',2,1),(38,'kafka',3,1);
+DROP TABLE IF EXISTS `user_token`;
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL DEFAULT '',
+  `token` varchar(255) NOT NULL DEFAULT '',
+  `get_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- src/test.py
+++ src/test.py
@@ -11,15 +11,23 @@ from src.function.class_method import *
 from src.common.redis import queue
 
 
-
-cum = queue.InitQ('zabbix_event_queue')
-
-warning_dict = {'description':'1.1.1.11PROBLEM: tomcat port 8080','source_id':1,'clock':1460962171}
-print warning_dict
-print cum.rpush(json.dumps(warning_dict))
+#
+# cum = queue.InitQ('zabbix_event_queue')
+#
+# warning_dict = {'description':'1.1.1.11PROBLEM: tomcat port 8080','source_id':1,'clock':1460962171}
+# print warning_dict
+# print cum.rpush(json.dumps(warning_dict))
 
 # a = Main()
 # res = a.sys_check(warning_dict, '10.17.26.185', 0, 2, 'ping')
 # print res
 # res = a.sys_check(warning_dict, '10.17.26.184', 0, 2, 'ping')
 # print res
+
+
+# root_child_path = '/route/docs_route.py'
+# root_api_path = os.getcwd() + root_child_path
+#
+# fh = open(root_api_path)
+# for line in fh.readlines():
+#     print line
